9ba74f49453dbf658407082adbea498e947afb31
Soumyajit Maity <64886400+Soumyajitm01@users.noreply.github.com>
2023-11-19 10:23:20 +0530

    Create 19.c

---
+++ 19.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main() 
+{
+    int evenNum[5] = {2,4,6,8,10};
+    for(int i=0; i<5; i++) {  
+        printf("%d element is %d\n", i, evenNum[i]);  
+    }  
+     return 0;  
+}  
