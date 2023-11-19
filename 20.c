61db124203a3af8af0fbde22f4ae35a82dbda889
Soumyajit Maity <64886400+Soumyajitm01@users.noreply.github.com>
2023-11-19 10:35:17 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  
+int main() 
+{
+    //The given array is of the first 5 even numbers
+    int evenNum[5] = {2,4,6,8,10};
+
+    //To print the second element in the array
+    printf("%d is the second element in the array",evenNum[1]);
+    
+    return 0;
+    }  
